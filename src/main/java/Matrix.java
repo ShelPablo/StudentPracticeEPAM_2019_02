7c2409549eeb1b5b7fa3ba90c3b46aac7c60a1b4
shel <shel-ne@mail.ru>
2019-02-04 11:30:35 +0300

    init commit

---
+++ src/main/java/Matrix.java
@@ -0,0 +1,27 @@
+import java.util.List;
+
+public interface Matrix {
+
+    double get(int rowIdx, int colIdx);
+
+    List<Double> getRow(int rowIdx);
+
+    List<Double> getCol(int colIdx);
+
+    /*transpose matrix*/
+    Matrix t();
+
+    /*element-by-element multiplication*/
+    Matrix dot(Matrix matrix);
+
+    /*row-on-column matrix multiplication*/
+    Matrix x(Matrix matrix);
+
+    /**
+     *
+     * @param dimension: 1 - rows; 2 - columns
+     * @return number of rows/columns
+     */
+    int getSize(int dimension);
+
+}
