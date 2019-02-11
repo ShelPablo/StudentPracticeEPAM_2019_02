f68502dbd271d1283982887990a7af3577ddb383
Aleksandr <grigorev.aleksandr@yahoo.com>
2019-02-11 16:08:05 +0300

    image loader done

--- src/main/java/imageprocessor/ImageProcessor.java
+++ src/main/java/imageprocessor/ImageProcessor.java
@@ -2,17 +2,24 @@ package imageprocessor;
 
 import matrix.Matrix;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public abstract class ImageProcessor {
 
-    private ImageLoader imageLoader;
+    //класс изображения(?) будет наследовать этот абстрактный класс и реализовывать loadImage - полная изоляция таким образом
 
-    private List<Matrix> image;
+    private ImageLoader imageLoader;//типо "объект интерфейса" - чтобы матрица не знала откуда она взялась
+
+    private List<Matrix> image;//возвращаемое значение  в размере 3 матриц!
 
     public List<Matrix> loadImage(String filename) {
+
         return image = imageLoader.loadImageAsMatrix(filename);
-    };
+    }
 
     public abstract void showImage();
 
---
+++ src/main/java/imageprocessor/ImageProcessorC.java
@@ -0,0 +1,17 @@
+package imageprocessor;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.List;
+
+public class ImageProcessorC extends ImageProcessor{
+
+
+    @Override
+    public void showImage() {
+
+
+
+    }
+}
