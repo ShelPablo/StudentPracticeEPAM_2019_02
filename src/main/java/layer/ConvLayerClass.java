7bfb0c1217c5ef8e4e0812b7f2342d762471f6ce
Bishamon186 <225122@niuitmo.ru>
2019-02-17 19:23:36 +0300

    Change ConvLayerClass

--- src/main/java/layer/ConvLayerClass.java
+++ src/main/java/layer/ConvLayerClass.java
@@ -7,15 +7,15 @@ import java.util.List;
 
 public abstract class ConvLayerClass implements Layer {
 
-    protected List<List<Matrix>> _kernel;
-    protected List<Matrix> _input;
-    protected List<Matrix> _output;
-    protected int _stair;
+    protected List<List<Matrix>> kernel;
+    protected List<Matrix> input;
+    protected List<Matrix> output;
+    protected int stride;
 
-    public ConvLayerClass(List<List<Matrix>> kernel, List<Matrix> input, int stair){
-        _kernel = kernel;
-        _input = input;
-        _stair = stair;
+    public ConvLayerClass(List<List<Matrix>> kernel, List<Matrix> input, int stride){
+        this.kernel = kernel;
+        this.input = input;
+        this.stride = stride;
     }
 
 
@@ -23,18 +23,11 @@ public abstract class ConvLayerClass implements Layer {
 
     }
 
-    public List<Matrix> convolute(List<Matrix> input) {
-        List<Matrix> res = new ArrayList<>();
-        for (List<Matrix> filter : _kernel) {
-            res.add((convolute3d(input, filter).get(0)));
-        }
-        return res;
-    }
 
     public List<Matrix> convolute3d(List<Matrix> input, List<Matrix> kernel) {
         //check layer's count
         if (input.size() != kernel.size()) {
-            throw new RuntimeException("Kernel and image have a different number of layer");
+            throw new IllegalArgumentException("Kernel and image have a different number of layer");
         }
 
         int height = kernel.get(0).getSize(1);
@@ -44,9 +37,9 @@ public abstract class ConvLayerClass implements Layer {
         List<List<Double>> resultList = new ArrayList<>();
 
 
-        for (int i = 0; i < input.get(0).getSize(1) / _stair; i++) {
+        for (int i = 0; i < input.get(0).getSize(1) / this.stride; i++) {
             List<Double> resultRow = new ArrayList<>();
-            for (int j = 0; j < input.get(0).getSize(2) / _stair; j++) {
+            for (int j = 0; j < input.get(0).getSize(2) / this.stride; j++) {
                 resultRow.add(0d);
             }
             resultList.add(resultRow);
@@ -57,17 +50,18 @@ public abstract class ConvLayerClass implements Layer {
             Matrix rgbInputMatrix = input.get(k);
             //row
 
-            for (int i = 0; i < (rgbInputMatrix.getSize(1) - ((rgbInputMatrix.getSize(1) % _stair) * _stair)) - height; i += _stair) {
+            for (int i = 0; i < (rgbInputMatrix.getSize(1) - ((rgbInputMatrix.getSize(1) % this.stride) * this.stride)) - height; i += this.stride) {
 
                 //col
-                for (int j = 0; j < (rgbInputMatrix.getSize(2) - ((rgbInputMatrix.getSize(2) % _stair) * _stair)) - height - width; j += _stair) {
+                for (int j = 0; j < (rgbInputMatrix.getSize(2) - ((rgbInputMatrix.getSize(2) % this.stride) * this.stride)) - height - width; j += this.stride) {
 
                     List<List<Double>> pieceInputMatrix = takePieceInputMatrix(rgbInputMatrix, width, height, i, j);
                     Matrix m = kernel.get(k).dot(new MatrixClass(pieceInputMatrix));
-                    if (k == input.size() - 1 && resultList.get(i / 4).get(j / 4) + Sum(m) < 0d) {
-                        resultList.get(i / _stair).set(j / _stair,0d);
+                    double value = resultList.get(i / 4).get(j / 4) + Sum(m);
+                    if (k == input.size() - 1 ) {
+                        resultList.get(i / this.stride).set(j / this.stride,ReLU(value));
                     } else {
-                        resultList.get(i / _stair).set(j / _stair,resultList.get(i / 4).get(j / 4) + Sum(m));
+                        resultList.get(i / this.stride).set(j / this.stride,value);
                     }
 
                 }
@@ -79,7 +73,17 @@ public abstract class ConvLayerClass implements Layer {
         return resMatrix;
     }
 
-    public List<List<Double>> takePieceInputMatrix(Matrix inputMatrix, int width, int height, int col, int row) {
+    private double ReLU(double value){
+        if(value<0){
+            return 0;
+        }
+        else
+        {
+            return value;
+        }
+    }
+
+    private List<List<Double>> takePieceInputMatrix(Matrix inputMatrix, int width, int height, int col, int row) {
 
         List<List<Double>> pieceInputMatrix = new ArrayList<>();
 
@@ -95,7 +99,7 @@ public abstract class ConvLayerClass implements Layer {
 
     }
 
-    public Double Sum(Matrix m) {
+    private Double Sum(Matrix m) {
 
         double res = 0d;
         for (int i = 0; i < m.getSize(1); i++) {
