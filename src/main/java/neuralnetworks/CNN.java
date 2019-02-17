044f4f264664a26259de79f443595843ceff5982
Pavel_Shelest <pavel_shelest@epam.com>
2019-02-17 17:46:42 +0300

    CNN corrected

--- src/main/java/neuralnetworks/CNN.java
+++ src/main/java/neuralnetworks/CNN.java
@@ -2,7 +2,13 @@ package neuralnetworks;
 
 public class CNN extends AbstractLayeredNeuralNetwork{
 
+    @Override
+    public void train(String pathToTrainingSet) {
 
+    }
 
+    @Override
+    public void test(String pathToTestSet) {
 
+    }
 }
