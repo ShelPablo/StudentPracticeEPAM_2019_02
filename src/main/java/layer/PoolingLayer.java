db677d12f915d87852cdc9aa4aec023dfe6ceb9b
Ann <Anya-Baburova@yandex.ru>
2019-02-17 20:37:16 +0300

    created PoolingLayer and added tests for layers

---
+++ src/main/java/layer/PoolingLayer.java
@@ -0,0 +1,66 @@
+package layer;
+
+import matrix.Matrix;
+import matrix.MatrixClass;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PoolingLayer implements Layer{
+
+    private int cellSize1;
+    private int cellSize2;
+    private List<Matrix> input;
+    private List<Matrix> output = new ArrayList<>();
+
+    public List<Matrix> getInput(){
+        return input;
+    }
+
+    public List<Matrix> getOutput(){
+        return output;
+    }
+
+    public PoolingLayer(int cellSize1, int cellSize2, List<Matrix> input){
+        this.cellSize1 = cellSize1;
+        this.cellSize2 = cellSize2;
+        this.input = input;
+    }
+
+    // Maximum function
+    private double getMaxValue(Matrix input, int indexOfRow, int indexOfCol){
+        double maxValue = 0;
+
+        for (int i = indexOfRow; i < (indexOfRow + cellSize1); i++) {
+            for (int j = indexOfCol; j < (indexOfCol + cellSize2); j++) {
+                if (input.get(i, j) > maxValue)
+                    maxValue = input.get(i, j);
+            }
+        }
+
+        return maxValue;
+    }
+
+    public void apply(){
+        for (Matrix matrix: input){
+            List<List<Double>> result = new ArrayList<>();
+            int rows = matrix.getSize(1) / cellSize1;
+            int columns = matrix.getSize(2) / cellSize2;
+
+            for (int i = 0; i < rows; i++) {
+                result.add(new ArrayList<>(Collections.nCopies(columns, 0.)));
+            }
+
+            for (int i = 0; i < rows; i++){
+                for (int j = 0; j < columns; j++){
+                    int indexOfRow = i * cellSize1;
+                    int indexOfCol = j * cellSize2;
+                    result.get(i).set(j, getMaxValue(matrix, indexOfRow, indexOfCol));
+                }
+            }
+
+            output.add(new MatrixClass(result));
+        }
+    }
+}
