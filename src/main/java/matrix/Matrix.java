6d60af6aaf5c6681299a01eefe67a70538398800
SorokinSemyon <semon.sorokin@gmail.com>
2019-02-21 00:55:49 +0300

    Change method fromString to static

--- src/main/java/matrix/Matrix.java
+++ src/main/java/matrix/Matrix.java
@@ -1,16 +1,22 @@
 package matrix;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface Matrix {
-
+    
     List<List<Double>> getMatrix();
 
+
     double get(int rowIdx, int colIdx);
+
+    void set(int rowIdx, int colIdx, double value);
 
     List<Double> getRow(int rowIdx);
 
     List<Double> getCol(int colIdx);
+
+    void setRow(int rowIdx, List<Double> value);
 
     /*transpose matrix*/
     Matrix t();
@@ -19,7 +25,10 @@ public interface Matrix {
     Matrix dot(Matrix matrix);
 
     /*row-on-column matrix multiplication*/
-    Matrix x(Matrix matrix);
+    Matrix x(Matrix matrix) ;
+
+    Double convolute(Matrix matrix);
+
 
     /**
      *
@@ -28,4 +37,30 @@ public interface Matrix {
      */
     int getSize(int dimension);
 
+
+    Matrix subMatrix(int rowIdx, int colIdx, int size1, int size2);
+
+
+    //Matrix from string
+    static Matrix fromString(String string){
+
+        List<List<Double>> matrixFromString = new ArrayList<>();
+
+        String[] stringMatrix = string.split(";'\n'");
+        for (String s: stringMatrix) {
+            String[] rowStringMatrix = s.split(",");
+            List<Double> rowFromString = new ArrayList<>();
+            for (String _s: rowStringMatrix){
+
+                double el = Double.parseDouble(_s);
+                rowFromString.add(el);
+            }
+            matrixFromString.add(rowFromString);
+        }
+
+        return new MatrixClass(matrixFromString);
+    }
 }
+
+
+
