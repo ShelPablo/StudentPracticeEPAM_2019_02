2e01527d63e7f0afa762b123d9ae4c304e4b48bf
SorokinSemyon <semon.sorokin@gmail.com>
2019-02-21 01:00:32 +0300

    Implementation of xWeightCoefs method

---
+++ src/main/java/neuralnetworks/SimpleCorrelator.java
@@ -0,0 +1,137 @@
+package neuralnetworks;
+
+import layer.Layer;
+import layer.conv.AlexConv1Layer;
+import layer.conv.Simple3dConvLayer;
+import layer.fully.FinalLayer;
+import layer.fully.FullyConnectedLayerBuilder;
+import layer.pool.MaxPoolLayer;
+import matrix.Matrix;
+import matrix.MatrixClass;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleCorrelator {
+
+    List<String> fileNames; // "rub50", "rub100", "rub200"
+
+    List<List<Matrix>> coefficientsSet;
+
+    List<Double> thresholds;
+
+    String weightsStr = "0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00;'\n'" +
+                         "0.00,0.25,0.37,0.50,0.62,0.73,0.80,0.82,0.82,0.80,0.73,0.62,0.50,0.37,0.25,0.00;'\n'" +
+                         "0.00,0.28,0.62,0.70,0.80,0.86,0.93,0.95,0.95,0.93,0.86,0.80,0.70,0.62,0.28,0.00;'\n'" +
+                         "0.00,0.29,0.63,0.72,0.80,0.88,0.96,1.00,1.00,0.96,0.88,0.80,0.72,0.63,0.29,0.00;'\n'" +
+                         "0.00,0.29,0.63,0.72,0.80,0.88,0.96,1.00,1.00,0.96,0.88,0.80,0.72,0.63,0.29,0.00;'\n'" +
+                         "0.00,0.28,0.62,0.70,0.80,0.86,0.93,0.95,0.95,0.93,0.86,0.80,0.70,0.62,0.28,0.00;'\n'" +
+                         "0.00,0.25,0.37,0.50,0.62,0.73,0.80,0.82,0.82,0.80,0.73,0.62,0.50,0.37,0.25,0.00;'\n'" +
+                         "0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00";
+    Matrix weights = Matrix.fromString(weightsStr);
+
+    private int trainingSetVolume = 0;
+
+    //64x128
+    Layer conv1 = new AlexConv1Layer();
+    //16x32x96
+    Layer pool1 = new MaxPoolLayer(2, 2);
+    //8x16x96
+    FinalLayer finalLayer = new FinalLayer();
+    //6x1
+
+
+    public List<Boolean> apply(List<Matrix> input) {
+        //conv1.apply
+        //pool1.apply
+        //finalLayer.apply
+
+        //getDecision
+        return null;
+    }
+
+    //Group - recognition class (100rub, 200rub ...)
+    public void trainCoefSetForGroup(List<Matrix> inputRGBimage, int groupIdx) {
+        //conv1.apply
+        //pool1.apply -> result
+        //(coef1 + coef2 +...)/N  (coef1 + coef2 +... +result)/(N+1) =>
+        // coefficientsSet = coefficientsSet*N/(N+1) + result/(N+1)
+        // N - trainingSetVolume
+
+
+
+    }
+/*
+    public List<Double> getSTD() {
+        //foreach Group
+        //    sum = 0
+        //  foreach image in TrainingSet
+        //      sum += calculateSquaredDeviationForInput
+        //
+        //sqrt(sum/N)
+        return null;
+    }
+
+    public List<Double> getMaxDeviation() {
+        //foreach Group
+        //  max = 0
+        //  foreach image in TrainingSet
+        //      max ?= calculateSquaredDeviationForInput
+        //   addToList(max)
+        //return List<max>
+        return null;
+    }
+*/
+
+    private List<Boolean> getDecision(List<Double> correlationCoefficients) {
+        List<Boolean> decisions = new ArrayList<>();
+        for (int i = 0; i < this.thresholds.size(); i++) {
+            decisions.add(correlationCoefficients.get(i) > this.thresholds.get(i)  );
+        }
+
+        return decisions;
+    }
+
+
+    public List<Double> getThresholds() {
+        //foreach Group
+        //  foreach image in TrainingSet
+        //     trainOutput =  apply().get(groupIdx)
+        //     min ?= trainOutput
+        //  addToList(min)
+        //return List
+        return null;
+    }
+
+
+
+
+
+    public void trainFinalLayer() {
+        //foreach Group
+        //  foreach image in TrainingSet
+        //      trainCoefSetForGroup
+        //xWeightCoefs
+        //finalLayer.setCoefs(coefficients)
+        //finalLayer.uploadCoefs
+    }
+
+    private void xWeightCoefs() {
+        //multiply coefs by weights, that are 0 near the border
+
+        List<List<Matrix>> newCoefSet = null;
+
+        //for each group
+        for (int i = 0; i < coefficientsSet.size(); i++) {
+            List<Matrix> newCoefSetRow = null;
+
+            //for each filter
+            for (int j = 0; j < coefficientsSet.get(i).size(); j++) {
+                newCoefSetRow.add(coefficientsSet.get(i).get(j).dot(weights));
+            }
+            newCoefSet.add(newCoefSetRow);
+        }
+        coefficientsSet = newCoefSet;
+    }
+
+}
