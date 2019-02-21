e3a3fccaa912275d5765a3d7e8439e2f50fefc8c
Aleksandr <grigorev.aleksandr@yahoo.com>
2019-02-21 16:50:54 +0300

    merging

--- src/main/java/layer/conv/Simple3dConvLayer.java
+++ src/main/java/layer/conv/Simple3dConvLayer.java
@@ -1,31 +1,31 @@
-package layer.conv;
-
-import layer.Layer;
-import matrix.Matrix;
-
-import java.util.List;
-
-public class Simple3dConvLayer extends ConvolutionLayer implements Layer {
-    public Simple3dConvLayer(int i, int i1, int i2, int i3) {
-    }
-
-    @Override
-    public Layer setInput(List<Matrix> input) {
-        return null;
-    }
-
-    @Override
-    public List<Matrix> apply(List<Matrix> input) {
-        return null;
-    }
-
-    @Override
-    public List<Matrix> getOutput() {
-        return null;
-    }
-
-    @Override
-    public List<Matrix> apply(List<Matrix> input) {
-        return null;
-    }
-}
+//package layer.conv;
+//
+//import layer.Layer;
+//import matrix.Matrix;
+//
+//import java.util.List;
+//
+//public class Simple3dConvLayer extends ConvolutionLayer implements Layer {
+//    public Simple3dConvLayer(int i, int i1, int i2, int i3) {
+//    }
+//
+//    @Override
+//    public Layer setInput(List<Matrix> input) {
+//        return null;
+//    }
+//
+//    @Override
+//    public List<Matrix> apply(List<Matrix> input) {
+//        return null;
+//    }
+//
+//    @Override
+//    public List<Matrix> getOutput() {
+//        return null;
+//    }
+//
+//    @Override
+//    public List<Matrix> apply(List<Matrix> input) {
+//        return null;
+//    }
+//}
--- src/main/java/neuralnetworks/SimpleCorrelator.java
+++ src/main/java/neuralnetworks/SimpleCorrelator.java
@@ -2,13 +2,16 @@ package neuralnetworks;
 
 import layer.Layer;
 import layer.conv.AlexConv1Layer;
-import layer.conv.Simple3dConvLayer;
+//import layer.conv.Simple3dConvLayer;
 import layer.fully.FinalLayer;
 import layer.fully.FullyConnectedLayerBuilder;
 import layer.pool.MaxPoolLayer;
 import matrix.Matrix;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class SimpleCorrelator {
 
@@ -16,9 +19,11 @@ public class SimpleCorrelator {
 
     List<List<Matrix>> coefficientsSet;
 
+    List<Double> thresholds;
+
     Matrix weights; // fromString([ 0 0 0 0 0 0 ...
-                                // 0 0.1 0.1 0.1 ...
-                                // 0 0.1 0.2 0.2 ...
+    // 0 0.1 0.1 0.1 ...
+    // 0 0.1 0.2 0.2 ...
 
     private int trainingSetVolume = 0;
 
@@ -31,10 +36,11 @@ public class SimpleCorrelator {
     //6x1
 
 
-    public List<Double> apply(List<Matrix> input) {
+    public List<Boolean> apply(List<Matrix> input) {
         //conv1.apply
         //pool1.apply
         //finalLayer.apply
+        //getDecision
         return null;
     }
 
@@ -45,7 +51,75 @@ public class SimpleCorrelator {
         //(coef1 + coef2 +...)/N  (coef1 + coef2 +... +result)/(N+1) =>
         // coefficientsSet = coefficientsSet*N/(N+1) + result/(N+1)
         // N - trainingSetVolume
+
+
+
     }
+/*
+    public List<Double> getSTD() {
+        //foreach Group
+        //    sum = 0
+        //  foreach image in TrainingSet
+        //      sum += calculateSquaredDeviationForInput
+        //
+        //sqrt(sum/N)
+        return null;
+    }
+    public List<Double> getMaxDeviation() {
+        //foreach Group
+        //  max = 0
+        //  foreach image in TrainingSet
+        //      max ?= calculateSquaredDeviationForInput
+        //   addToList(max)
+        //return List<max>
+        return null;
+    }
+*/
+
+    private List<Boolean> getDecision(List<Double> correlationCoefficients) {
+        List<Boolean> decisions = new ArrayList<>();
+        for (int i = 0; i < this.thresholds.size(); i++) {
+            decisions.add(correlationCoefficients.get(i) > this.thresholds.get(i)  );
+        }
+
+        return decisions;
+    }
+
+
+    public List<Double> getThresholds() {
+
+        List<Boolean> trainOutput = new ArrayList<>();
+        File folder = new File("/src/main/java/resources");
+        File[] folderEntries = folder.listFiles();
+        int x = 0;
+        for (File entry : Objects.requireNonNull(folderEntries))
+        {
+            if (entry.isDirectory())
+            {
+                x++;
+                File[] collections = entry.listFiles();
+                int y = 0;
+                for (File picture : Objects.requireNonNull(collections))
+                {
+                    if (entry.isDirectory()){
+                        y++;
+
+                    }
+                }
+            }
+        }
+        //applywithoutdecision - list double
+        //foreach Group
+        //  foreach image in TrainingSet
+        //     trainOutput =  apply().get(groupIdx)
+        //     min ?= trainOutput
+        //  addToList(min)
+        //return List
+        return null;
+    }
+
+
+
 
 
     public void trainFinalLayer() {
