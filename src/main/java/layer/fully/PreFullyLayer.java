d797c1ca064c1eeea12c66697fa886ae51042045
Pavel_Shelest <pavel_shelest@epam.com>
2019-02-26 20:46:07 +0300

    init fully layers

--- src/main/java/layer/fully/PreFullyLayer.java
+++ src/main/java/layer/fully/PreFullyLayer.java
@@ -6,20 +6,38 @@ import java.util.List;
 
 public class PreFullyLayer {
 
-    //8x16x10
+    //8x16x(96+1)
     List<Matrix> input;
-    //8x16x10
+    //8x16x(96+1)
     List<List<List<Double>>> coefficients;
     //128  (8x16)
     List<Double> output;
 
-    double trainingRate;
+    private double trainingRate = 0.1;
 
-    PreFullyLayer train(List<Matrix> input, List<Double> trueOutput){
-        //formula
-        return this;
+    public List<Double> apply(List<Matrix> input) {
+        //convolute by 3-rd dimension
+        //8x16
+        //foreach sigmoid
+        return output;
     }
 
-    //void upload/download coefficients
+
+
+    List<Matrix> train(List<Double> deltaY){
+        //save input
+        //apply
+        //save output
+
+        //formula
+        return null;//deltaX;
+
+    }
+
+    void seedCoefficients() {
+        //random values in [-0.5 0.5]
+    };
+    void uploadCoefficients() {};
+    void downloadCoefficients() {};
 
 }
